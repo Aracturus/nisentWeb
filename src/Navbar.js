0c607912785eda870a655b83c5c2b6dc5f7743cc
Swati Mandal <Swatimandal01@gmail.com>
2021-10-11 14:47:14 +0530

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,16 +4,187 @@ import './navbar.css'
 
 // import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
- 
-    function toggleBurger() {
-      var burger = $('.burger');
-      var menu = $('.navbar-menu');
-      burger.toggleClass('is-active');
-      menu.toggleClass('is-active');
-  }
+const Navbar = () => { 
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-actives');
+          $target.classList.toggle('is-actives');
+  
+        });
+      });
+    }
+  
+  });
+    
   return (
     <>
+      {/* <div className="container-fluid px-5 py-3 nav-background">
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <div className="container-fluid">
+            <NavLink
+              className="navbar-brand"
+              activeClassName="navbar-active-class"
+              to="#"
+            >
+              <div>
+                <img src={logo} alt="Home Logo" className="logoStyle" />
+              </div>
+            </NavLink>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav mt-lg-4 mb-lg-0 w-100 d-flex justify-content-end">
+                <li className="nav-item ps-2">
+                  <NavLink
+                    className="nav-link pe-5"
+                    to="/"
+                    activeClassName="active-navbar"
+                  >
+                    Products
+                  </NavLink>
+                </li>
+                <li className="nav-item ps-2">
+                  <NavLink
+                    className="nav-link pe-5"
+                    to="/Products"
+                    activeClassName="active-navbar"
+                  >
+                    Digitalization
+                  </NavLink>
+                </li>
+                <li className="nav-item ps-2">
+                  <NavLink className="nav-link pe-5 " to="#">
+                    Engineering
+                  </NavLink>
+                  <ul
+                    className="sub-menus"
+                    aria-labelledby="dropdownMenuButton1"
+                  >
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Engineering Services
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Rotor Dynamics
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Surge Dynamics
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Reverse Engineering
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Retrofit & upgrade
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item ps-2 pe-5">
+                  <NavLink
+                    className="nav-link"
+                    to="#"
+                    data-bs-toggle="dropdown"
+                  >
+                    Plant Services
+                  </NavLink>
+                  <ul className="sub-menus">
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Operation and Maintanence
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Condition Monitoring
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Plant Turnaround
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Field Performance Test
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Engineered Spare Parts
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item ps-2 pe-5">
+                  <NavLink className="nav-link" to="#">
+                    Perspectives
+                  </NavLink>
+                </li>
+                <li className="nav-item ps-2">
+                  <NavLink className="nav-link pe-5 me-lg--5 " to="#">
+                    Company
+                  </NavLink>
+                  <ul className="sub-menus last">
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        About Us
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Careers
+                      </NavLink>
+                    </li>
+                    <li>
+                      <NavLink class="dropdown-item" to="#">
+                        Contact Us
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div> */}
+
+
 
 <nav className="navbar navbar-menu  is-black" id="nav-bar" role="navigation" aria-label="main navigation">
                 <div className="container ">
@@ -23,7 +194,7 @@ const Navbar = () => {
                       <img src={logo} width="180" height="40"/>
                     </a>
 
-                    <a href="/"role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <a href="/"role="button" className="navbar-burger  navbar-end" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                       <span aria-hidden="true"></span>
                       <span aria-hidden="true"></span>
                       <span aria-hidden="true"></span>
@@ -113,6 +284,7 @@ const Navbar = () => {
 
             </nav>
 
+            
     </>
   );
 };
